95ccbc9750469065d3d545c47fc09bfb7f3bfe48
Grzegorz Poplawski <rapatatui@gmail.com>
2025-06-04 16:52:26 +0200

    1

--- app/summerizer.py
+++ app/summerizer.py
@@ -2,10 +2,12 @@ from agno.agent import Agent
 from agno.app.fastapi.app import FastAPIApp
 from agno.app.fastapi.serve import serve_fastapi_app
 from agno.models.openrouter import OpenRouter
+from pydantic import BaseModel, Field
 
 class Summary(BaseModel):
     summary: str = Field(..., description="Transcription summary in markdown format")
     topic: str = Field(..., description="Main topic of the transcription")
+    
 basic_agent = Agent(
     name="Basic Agent",
     model=OpenRouter(id="google/gemini-2.5-flash-preview-05-20"), 
